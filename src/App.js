0e1d81f7eff588612c0a029247f1e0a2168848f5
romarios1987 <romios047@gmail.com>
2022-10-26 19:28:40 +0300

    Alert component

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import About from './pages/About';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
+import Alert from './components/layout/Alert';
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Alert />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
---
+++ src/components/layout/Alert.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { FiAlertCircle } from 'react-icons/fi';
+import AlertContext from '../../context/alert/AlertContext';
+
+const Alert = () => {
+  const { alert } = useContext(AlertContext);
+
+  return (
+    <div
+      className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-5 gap-8"
+      style={{ visibility: alert ? 'visible' : 'hidden' }}
+    >
+      <div className={`alert shadow-l alert-${alert?.type}`}>
+        <div>
+          <FiAlertCircle size="24" />
+          <strong>{alert?.message}</strong>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
